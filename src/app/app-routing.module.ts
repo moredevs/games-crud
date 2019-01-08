d571f50968f21f4e7c78533efb1380ee4a4640f8
Paul Santibanez <psantibanez@inove.local>
2019-01-08 12:31:52 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {GameListComponent} from './components/game-list/game-list.component'
+import {GameFormComponent} from './components/game-form/game-form.component'
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/juegos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'juegos',
+    component:GameListComponent
+  },
+  {
+    path:'juegos/agregar',
+    component: GameFormComponent
+  },
+  {
+    path:'juegos/editar/:id',
+    component: GameFormComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
